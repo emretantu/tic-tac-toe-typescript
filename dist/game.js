107ced277c25c8e12c482394bec83812032ffe63
emretantu <emretantu@gmail.com>
2025-12-25 04:28:55 +0300

    Implement checkWinner function and update basic data types

--- dist/game.js
+++ dist/game.js
@@ -1,8 +1,16 @@
-let board = [
-    null, null, null,
-    null, null, null,
-    null, null, null
-];
+var Player;
+(function (Player) {
+    Player["X"] = "X";
+    Player["Y"] = "Y";
+})(Player || (Player = {}));
+;
+var Status;
+(function (Status) {
+    Status["XWins"] = "X";
+    Status["YWins"] = "Y";
+    Status["Tie"] = "TIE";
+    Status["Ongoing"] = "ONGOING";
+})(Status || (Status = {}));
 const makeMove = (board, player, coordinate) => {
     if (board[coordinate] !== null) {
         return board;
@@ -11,8 +19,21 @@ const makeMove = (board, player, coordinate) => {
     nextBoard[coordinate] = player;
     return nextBoard;
 };
-console.log("First", board);
-board = makeMove(board, "X", 8);
-console.log("Last", board);
+const checkWinner = (board) => {
+    const winnerPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    for (const [first, second, third] of winnerPatterns) {
+        if (board[first] && (board[first] === board[second]) && (board[first] === board[third])) {
+            return board[first];
+        }
+    }
+    if (!board.includes(null)) {
+        return Status.Tie;
+    }
+    return Status.Ongoing;
+};
 export {};
 //# sourceMappingURL=game.js.map
--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,7 @@
-type Player = "X" | "O";
+enum Player {
+  X = "X",
+  Y = "Y"
+};
 type Coordinates = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 type Cell = Player | null;
 type Board = [
@@ -6,12 +9,13 @@ type Board = [
   Cell, Cell, Cell,
   Cell, Cell, Cell
 ];
-
-let board: Board = [
-  null, null, null,
-  null, null, null,
-  null, null, null
-]
+enum Status {
+  XWins = Player.X,
+  YWins = Player.Y,
+  Tie = "TIE",
+  Ongoing = "ONGOING"
+} 
+type GameState = { board: Board, status: Status, currentPlayer: Player }
 
 const makeMove = (
   board: Board,
@@ -19,15 +23,28 @@ const makeMove = (
   coordinate: Coordinates
 ): Board => {
   if (board[coordinate] !== null) {
-    return board; // ??? NULL? STATUS?
+    return board; // same object/reference
   }
   const nextBoard: Board = [...board];
   nextBoard[coordinate] = player;
   return nextBoard;
 };
 
-console.log("First", board);
-
-board = makeMove(board, "X", 8);
-
-console.log("Last", board);
+const checkWinner = (
+  board: Board
+): Status  => {
+  const winnerPatterns: [Coordinates, Coordinates, Coordinates][] = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
+    [0, 4, 8], [2, 4, 6] // Diagonal
+  ]
+  for (const [first, second, third] of winnerPatterns) {
+    if (board[first] && (board[first] === board[second]) && (board[first] === board[third])) {
+      return board[first] as unknown as Status;
+    }
+  }
+  if (!board.includes(null)) {
+    return Status.Tie;
+  }
+  return Status.Ongoing;
+};
